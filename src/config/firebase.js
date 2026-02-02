18698218fcacc096c19aef6b8e0a479133c8c6d9
grishmaa7 <gisme7@gmail.com>
2026-02-02 10:26:33 +0545

    initial commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,8 @@
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,16 +10,16 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "e-commerce-900e2.firebaseapp.com",
-  projectId: "e-commerce-900e2",
-  storageBucket: "e-commerce-900e2.firebasestorage.app",
-  messagingSenderId: "631043805545",
-  appId: "1:631043805545:web:bbd26ddf6dff566a4f6df8",
-  measurementId: "G-H8DTM2MYJD"
+  authDomain: "e-commerce-9650d.firebaseapp.com",
+  databaseURL: "https://e-commerce-9650d-default-rtdb.firebaseio.com",
+  projectId: "e-commerce-9650d",
+  storageBucket: "e-commerce-9650d.firebasestorage.app",
+  messagingSenderId: "441329094394",
+  appId: "1:441329094394:web:334a362cf8a2a6eae08343",
+  measurementId: "G-4YMGT3F2VP"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
